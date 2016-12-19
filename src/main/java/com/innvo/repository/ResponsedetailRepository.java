2d5d5a0982372a1d4a14b2198083a30fe8d91947
arun-git <arun@mistminds.com>
2016-12-19 21:18:59 +0530

    ADAP-148 #comment Implemented the code for getting the response from responsembr and responsedetail

--- src/main/java/com/innvo/repository/ResponsedetailRepository.java
+++ src/main/java/com/innvo/repository/ResponsedetailRepository.java
@@ -3,6 +3,7 @@ package com.innvo.repository;
 import com.innvo.domain.Responsedetail;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +13,6 @@ import java.util.List;
 @SuppressWarnings("unused")
 public interface ResponsedetailRepository extends JpaRepository<Responsedetail,Long> {
 
+	@Query("SELECT d FROM Responsedetail d WHERE d.responseId=:responseId")
+	public List<Responsedetail> findByResponseId(@Param("responseId") long responseId);
 }
